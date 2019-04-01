c5fd5dd3f336cabcb02eaa074709cd02f7ce9f13
SandyQiu <37303878+sqiu001@users.noreply.github.com>
2019-04-01 01:55:45 -0400

    Add files via upload

---
+++ code/chap09/extra_credits/fib.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+void fib(int n){
+    if(n<=0){
+        return;
+    }
+    else{
+        cout<<"\t";
+        fib(n-1);
+    }
+}
+
+void tree(int n, int count){
+    if(n==0){
+        return;
+    }
+    fib(count+1);
+    cout<<"this was written by call number "<<count<<endl;
+    tree(n-1, count+1);
+    cout<<"this was ALSO written by call number " <<count<<endl;
+}
+
+int main(){
+    int num;
+    cout<<"enter number:";
+    cin>>num;
+    tree(num,0);
+    return 0;
+}
